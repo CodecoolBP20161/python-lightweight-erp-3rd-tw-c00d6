c6ac5d49f2276b568536e59ce24a67ae1b3442ad
CsibiJani <jozsef.berko.ifj@gmail.com>
2016-06-06 14:07:01 +0200

    menu 1.0 is ready

--- tool_manager/tool_manager.py
+++ tool_manager/tool_manager.py
@@ -21,10 +21,30 @@ common = SourceFileLoader("module.name", current_file_path + "/../common.py").lo
 
 # start this manager by a menu
 def start():
+    menu_options = ["Show tool table",
+                    "Add tool",
+                    "Remove tool",
+                    "Update tool",
+                    "Get available tools",
+                    "Get average durability by manufacturers"]
+    while True:
+        ui.print_menu("Tool Manager", menu_options, "Back to Main menu")
+        option = ui.get_inputs(list_titles, title)
+        if option == "1":
+            show_table(data_manager.get_table_from_file("tools.csv")
+        elif option == "2":
+            data_manager.write_table_to_file(add(data_manager.get_table_from_file("tools.csv")))
+        elif option == "3":
+            data_manager.write_table_to_file(remove(data_manager.get_table_from_file("tools.csv")))
+        elif option == "4":
+            data_manager.write_table_to_file(update(data_manager.get_table_from_file("tools.csv")))
+        elif option == "5":
+            data_manager.write_table_to_file(get_available_tools(data_manager.get_table_from_file("tools.csv")))
+        elif option == "6":
+            data_manager.write_table_to_file(get_average_durability_by_manufacturers(data_manager.get_table_from_file("tools.csv")))
+        elif option == "0":
+            break
 
-    # you code
-
-    pass
 
 
 # print the default table of records from the file
@@ -37,7 +57,7 @@ def show_table(table):
 
 # Ask a new record as an input from the user than add it to @table, than return @table
 def add(table):
-
+    list_of_titles = ["Please enter the person's name: ", "Please enter the manufacturer's name:", "Please enter the purchase date:", "Please enter the durability of the product:"]
     # your code
 
     return table
